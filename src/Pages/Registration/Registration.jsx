763cd03b61501811b5f1baac1e3b54d90beb8360
Sirajul Islam <ayasabegum134@gmail.com>
2023-10-07 07:37:42 +0600

    I complete Registration Authentication

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../Components/ReUsed/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Registration = () => {
+    const {createUser} = useContext(AuthContext)
+    const registrationHandle = (e) =>{
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const password = form.get('password');
+        createUser(email, password)
+        .then(response => {
+            console.log(response.user)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
     return (
         <div className="mt-5 bg-[#F3F3F3]">
             <div className="">
@@ -11,7 +27,7 @@ const Registration = () => {
                 <div className="bg-white px-24 w-[600px] mx-auto py-5">
                     <h3 className="text-4xl font-semibold text-center">Register your account</h3>
                     <hr className="my-4" />
-                    <form>
+                    <form onSubmit={registrationHandle}>
                         <div className="form-control w-full px-5 mb-3">
                             <label className="label">
                                 <span className="label-text text-xl font-semibold">Your Name</span>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,16 @@
 import { createContext } from "react";
 import app from "../Firebase/firebase.config";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
     const myAuth = {
-
+        createUser
     }
 
     return (
